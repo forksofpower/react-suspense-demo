c5e2953bce70ce01e494a6538747698410401c74
Patrick Jones <patrick.jones@auctane.com>
2022-06-14 02:25:21 -0500

    add `wrapPromise` helper

---
+++ src/wrapPromise.ts
@@ -0,0 +1,33 @@
+enum FetchStatus {
+  pending = 'PENDING',
+  success = 'SUCCESS',
+  error = 'ERROR',
+}
+
+export const wrapPromise = <T = any>(promise: Promise<T>) => {
+  let status: FetchStatus = FetchStatus.pending;
+  let result: T;
+
+  let suspender = promise.then(
+    (res) => {
+      status = FetchStatus.success;
+      result = res;
+    },
+    (err) => {
+      status = FetchStatus.error;
+      result = err;
+    }
+  );
+  return {
+    read() {
+      switch (status) {
+        case FetchStatus.pending:
+          throw suspender;
+        case FetchStatus.error:
+          throw result;
+        case FetchStatus.success:
+          return result;
+      }
+    },
+  };
+};
